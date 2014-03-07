895045ab535fb26518fcce056e30be3142840739
Jim Friend <jimfriend15@gmail.com>
2014-03-07 09:17:06 -0800

    Base files from my PHP and Twitter bootstrap starter framework

---
+++ includes/footer.php
@@ -0,0 +1,15 @@
+			<hr>
+	
+			<footer>
+				<p>&copy; Company 2013</p>
+			</footer>
+		</div> <!-- /container -->
+
+		<!-- Modal -->
+		<?php require_once('login-modal.php'); ?>
+		<?php require_once('register-modal.php'); ?>
+		
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+		<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
+	</body>
+</html>
